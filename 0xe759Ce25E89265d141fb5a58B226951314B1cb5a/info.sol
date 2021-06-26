80b6272f4e2cf4fff951727c9a45fe6def137883
xiaolychee <85621081+xiaolychee@users.noreply.github.com>
2021-06-27 05:58:54 +0800

    Add BabyOtter logo and info

---
+++ 0xe759Ce25E89265d141fb5a58B226951314B1cb5a/info.sol
@@ -0,0 +1,11 @@
+{
+ "name": " BabyOtter",
+ "website": "",
+ "description": "",
+ "explorer": "https://bscscan.com/token/0xe759Ce25E89265d141fb5a58B226951314B1cb5a",
+ "type": "BEP20",
+ "symbol": "BabyOtter",
+ "decimals": 9,
+ "status": "active",
+ "id": "0xe759Ce25E89265d141fb5a58B226951314B1cb5a"
+}
