7dbab77807b47cba70dc295ce096986cd0fc3003
pp13272727056 <84385417+pp13272727056@users.noreply.github.com>
2021-07-27 11:02:20 +0800

    add ABC logo and info

---
+++ bsc/0xEF135D1098E4c0156ab71278145375Ab81FC526F/LUCKYUNI.sol
@@ -0,0 +1,12 @@
+{
+ "name": "LUCKYUNI",
+ "website": "http//www.luckyuni.com",
+ "description": "LUCKYUNI"
+HappyDoge is a yield-generating deflationary charitable cryptocurrency that runs on Binance Smart Chain. It rewards holders through reflections by taxing transactions. It charity is dedicated in dogs protection and creating dog shelter ",
+ "explorer": "https://bscscan.com/token/0xEF135D1098E4c0156ab71278145375Ab81FC526F",
+ "type": "BEP20",
+ "symbol": " LUCKYUNI",
+ "decimals": 18,
+ "status": "active",
+ "id": "0xEF135D1098E4c0156ab71278145375Ab81FC526F"
+}
