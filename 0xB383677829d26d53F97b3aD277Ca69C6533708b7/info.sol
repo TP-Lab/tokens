ecf53220e84cc64f59bc632583b2a2f95337c625
Poor Travel Meta <94161369+PoorTravelMeta@users.noreply.github.com>
2021-11-16 13:34:35 +0800

    Add files via upload

---
+++ 0xB383677829d26d53F97b3aD277Ca69C6533708b7/info.sol
@@ -0,0 +1,11 @@
+{
+ "PoorTravelMeta": "TokenPocket Token",
+ "website": "https://www.poortravelmeta.com",
+ "description": "A leading multi-chain wallet that supports BTC, ETH, BSC, HECO, TRON, EOS, OKExChain, HSC and so on.",
+ "explorer": "https://www.bscscan.com/token/0xB383677829d26d53F97b3aD277Ca69C6533708b7",
+ "type": "BEP20",
+ "symbol": "PTM",
+ "decimals": 9,
+ "status": "active",
+ "id": "0xB383677829d26d53F97b3aD277Ca69C6533708b7"
+}
