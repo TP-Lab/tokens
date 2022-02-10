77877918eb63e3880811850b8cf11a5c2cce812d
CBTdao <99331298+CBTdao@users.noreply.github.com>
2022-02-10 22:16:42 +0800

    ADD CBT logo and info
    https://bscscan.com/tx/0xb14fee7196704c13c7231fdeb20da123dabd0a301a4b93945ef80866ab1f7e2c

---
+++ 0xA36440bbA77a0d5b171842808FA242A4323ad530/info.sol
@@ -0,0 +1,11 @@
+{
+ "name": "cryptoburn Token",
+ "website": "https://Cryptoburn.CC",
+ "description": "crypto burn token.",
+ "explorer": "https://bscscan.com/token/0xA36440bbA77a0d5b171842808FA242A4323ad530",
+ "type": "BEP20",
+ "symbol": "CBT",
+ "decimals": 18,
+ "status": "active",
+ "id": "0xA36440bbA77a0d5b171842808FA242A4323ad530"
+}
